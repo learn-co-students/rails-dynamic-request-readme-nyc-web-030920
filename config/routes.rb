2d5347851f2e003208ec052d4f71e507e4d43b8b
MisterC1ean <robert.keller22@gmail.com>
2020-04-02 09:17:45 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'posts/:id', to: 'posts#show'
+  # Refactor line 3 to ...
+  resources :posts, only: :show
 end
